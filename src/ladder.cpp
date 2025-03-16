a8be2fc3b5ccc1e29628add8b48dc3dbf4b43785
Kanec Olivares <kaneco@uci.edu>
2025-03-16 23:04:07 +0000

    Print fixed.

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -91,6 +91,7 @@ void load_words(set<string> & word_list, const string& file_name){
 void print_word_ladder(const vector<string>& ladder){
     if (ladder.empty()){
         cout << "No word ladder found." << endl;
+        return;
     }
     cout << "Word ladder found: ";
     for (int i = 0; i < ladder.size(); ++i){
